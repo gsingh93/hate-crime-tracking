62ff57a92c67bf8f6dffc3e0d451e3120949af74
Gulshan Singh <gsingh2011@gmail.com>
2016-11-10 19:18:58 -0800

    Fixed indentation and lint errors in location.js

--- location.js
+++ location.js
@@ -1,39 +1,38 @@
-  function map_generation(markerCenter){
-      var mapProp = {
-          center: markerCenter,
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-      var map = new google.maps.Map(document.getElementById("map"),mapProp);
+function map_generation(markerCenter){
+  var mapProp = {
+    center: markerCenter,
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapProp);
 
-      return map;
-    }
+  return map;
+}
 
-  google.maps.event.addDomListener(window, 'load', function () {
-    var options = {
-      types: ['(cities)']
-    };
-    var input = document.getElementById('autocomplete');
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
-    var markerCenter = new google.maps.LatLng(-77.85,166.666667);
+google.maps.event.addDomListener(window, 'load', function () {
+  var options = {
+    types: ['(cities)']
+  };
+  var input = document.getElementById('autocomplete');
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+  var markerCenter = new google.maps.LatLng(-77.85,166.666667);
+  var marker = null;
+
+  var map = map_generation(markerCenter);
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
+
+    var place = autocomplete.getPlace();
+    var latitude = place.geometry.location.lat();
+    var longitude = place.geometry.location.lng();
+    var markerCenter = new google.maps.LatLng(latitude, longitude);
     var marker = null;
 
-    var map = map_generation(markerCenter)
+    var map = map_generation(markerCenter);
 
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-
-      var place = autocomplete.getPlace();
-      var latitude = place.geometry.location.lat();
-      var longitude = place.geometry.location.lng();
-      var markerCenter = new google.maps.LatLng(latitude,longitude);
-      var marker = null;
-
-      var map = map_generation(markerCenter)
-
-      var marker = new google.maps.Marker({
-         position: markerCenter,
-      });
-      marker.setMap(map);
-
+    var marker = new google.maps.Marker({
+      position: markerCenter
     });
+    marker.setMap(map);
   });
+});

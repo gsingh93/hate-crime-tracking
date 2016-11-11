6fb930f40ffe8ca8a93e49e19eebbc7a529e03d4
Amaraj Judge <amaraj@umich.edu>
2016-11-11 15:29:44 -0800

    started routing with flask, and edited Gruntfile so index.html is generated in templates folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 	      }
 	    },
 	    files: {
-	      'index.html': ['index.jade']
+	      'templates/index.html': ['index.jade']
 	    }
 	  }
 	},
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+
+        return render_template('index.html')
+
+    elif request.method == 'POST':
+
+        for key, value in request.form.items():
+
+            print(key, value)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()

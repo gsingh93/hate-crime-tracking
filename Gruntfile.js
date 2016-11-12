faaf516fc803afa03c0abf7fe1b4cdb30fd43e16
Gulshan Singh <gsingh2011@gmail.com>
2016-11-11 21:13:38 -0800

    Reorganized directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,49 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
   grunt.initConfig({
-  	pkg: grunt.file.readJSON('package.json'),
-	pug: {
-	  compile: {
-	    options: {
-	      data: {
-	        debug: false
-	      }
-	    },
-	    files: {
-	      'templates/index.html': ['index.jade']
-	    }
-	  }
-	},
-	concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['*.js', '!Gruntfile.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
+    pug: {
+      compile: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'dist/templates/index.html': ['src/static/jade/index.jade']
         }
       }
     },
+    copy: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['app.py'],
+            dest: 'dist'
+          },
+        ]
+      },
+      boostrap: {
+        files: [{
+            expand: true,
+            cwd: 'node_modules/bootstrap/dist/css',
+            src: ['boostrap.min.css'],
+            dest: 'dist/static'
+          }
+        ]
+      }
+    },
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/static/script',
+          src: '*.js',
+          dest: 'dist/static'
+        }]
+      }
+    },
     jshint: {
-      files: ['Gruntfile.js', '*.js'],
+      files: ['Gruntfile.js', 'src/static/script/*.js'],
       options: {
-        // options here to override JSHint defaults
         globals: {
           jQuery: true,
           console: true,
-          module: true,
           document: true
         }
       }
@@ -49,27 +52,22 @@ module.exports = function(grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: 'styles', //need to change to whatever folder will hold sass files
+          cwd: 'src/static/scss',
           src: ['*.scss'],
-          dest: '../public', //need to change to destination for the css files
+          dest: 'dist/static',
           ext: '.css'
         }]
       }
-  	}
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-pug');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
-
-
 
   grunt.registerTask('compile',  ['pug']);
   grunt.registerTask('test', ['jshint']);
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'pug', 'sass']);
-
-
-
+  grunt.registerTask('default', ['jshint', 'uglify', 'pug', 'sass', 'copy']);
 };
